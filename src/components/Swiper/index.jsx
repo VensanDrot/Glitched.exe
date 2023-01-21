2636b5b04178cc4c86ef17ed9ae254b4fb4f9aab
VensanDrot <mrvenchannel@gmail.com>
2023-01-21 14:09:20 -0500

    Updates

--- src/components/Swiper/index.jsx
+++ src/components/Swiper/index.jsx
@@ -4,21 +4,25 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 import "./index.css";
 
 // import required modules
-import { Pagination } from "swiper";
-
+import { Pagination, Navigation } from "swiper";
 const SwiperRes = () => {
   return (
     <>
       <Swiper
         slidesPerView={3}
         spaceBetween={30}
+        loop={true}
+        centeredSlides={true}
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination]}
+        navigation={true}
+        modules={[Pagination, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide>Slide 1</SwiperSlide>
