26ff8f30c5681c66e43ee31026fae4e086504100
Vensan <v.drot@student.inha.uz>
2023-01-26 19:16:33 +0500

    hamburger

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./index.css";
 import logo from "./logo.svg";
+import { Sling as Hamburger } from "hamburger-react";
 
 const Header = () => {
+  const [isOpen, setOpen] = useState(false);
   return (
     <>
       <div className="header_container">
@@ -12,6 +14,9 @@ const Header = () => {
             <img src={logo} alt="" />
             Glitched.exe
           </div>
+          <div className="ham">
+            <Hamburger toggled={isOpen} toggle={setOpen} />
+          </div>
           <div className="links">
             <Link>О Нас</Link>
             <Link>Турниры</Link>
@@ -19,6 +24,12 @@ const Header = () => {
             <Link>Контанты</Link>
           </div>
         </div>
+      </div>
+      <div className={`${isOpen ? "ham_menu_active" : ""} ham_menu`}>
+        <Link onClick={() => setOpen(!isOpen)}>О Нас</Link>
+        <Link onClick={() => setOpen(!isOpen)}>Турниры</Link>
+        <Link onClick={() => setOpen(!isOpen)}>Партнеры</Link>
+        <Link onClick={() => setOpen(!isOpen)}>Контанты</Link>
       </div>
     </>
   );
