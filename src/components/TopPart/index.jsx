fcc30f47472a6e1d6f3005b98d1a923e861347b3
VensanDrot <mrvenchannel@gmail.com>
2023-02-11 08:56:35 -0500

    Video back

--- src/components/TopPart/index.jsx
+++ src/components/TopPart/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./index.css";
 import logo from "../Assets/logo.png";
+import video from "../Assets/glihed_exe.mp4";
 
 const TopPart = () => {
   return (
     <div className="parent">
+      <video autoPlay loop muted src={video} className="b_v"></video>
       <div className="hold">
         <img src={logo} alt="" />
         <div className="h1">
