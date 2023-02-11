7e8d56933b23e92ab0bca060d242f88c701a4fab
VensanDrot <mrvenchannel@gmail.com>
2023-02-11 09:28:27 -0500

    Top part update

--- src/components/TopPart/index.jsx
+++ src/components/TopPart/index.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./index.css";
-import logo from "../Assets/logo.png";
 import video from "../Assets/glihed_exe.mp4";
 
 const TopPart = () => {
   return (
     <div className="parent">
-      <video autoPlay loop muted src={video} className="b_v"></video>
+      <video className="b_v" autoPlay loop muted>
+        <source src={video} type="video/mp4" />
+      </video>
       <div className="hold">
-        <img src={logo} alt="" />
         <div className="h1">
-          <h1>Glitched</h1>
-          <h1>.exe</h1>
+          <h1>Gaming community with an artistic touch</h1>
+          <p>
+            We organize esports tournaments and related events, and also actively assist in the development of esports
+            in Uzbekistan
+          </p>
         </div>
       </div>
     </div>
