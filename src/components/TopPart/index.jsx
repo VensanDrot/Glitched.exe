295f190e00b43884f042fe355d4f29007bd6f847
VensanDrot <mrvenchannel@gmail.com>
2023-02-11 09:36:57 -0500

    Top part update

--- src/components/TopPart/index.jsx
+++ src/components/TopPart/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./index.css";
-import video from "../Assets/glihed_exe.mp4";
+import video from "../Assets/glihed_exe.webm";
 
 const TopPart = () => {
   return (
     <div className="parent">
       <video className="b_v" autoPlay loop muted playsinline>
-        <source src={video} type="video/mp4" />
+        <source src={video} type="video/webm" />
       </video>
       <div className="hold">
         <div className="h1">
