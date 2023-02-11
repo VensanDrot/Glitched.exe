d53427f5586e61e4ec1063d5ff257aff6701f83b
VensanDrot <mrvenchannel@gmail.com>
2023-02-11 12:50:27 -0500

    Changed structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { useState, useRef, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Redirect as redirect } from "react-router-dom";
 import Header from "./components/Header";
-import MainPage from "./pages/MainPage";
 import Footer from "./components/Footer";
 import SwiperRes from "./components/Swiper";
+//Pages
+import MainPage from "./pages/MainPage";
+import NotFoundPage from "./pages/NotFound";
 
 function App() {
   const [windowSize, setWindowSize] = useState([window.innerWidth, window.innerHeight]);
@@ -27,6 +30,7 @@ function App() {
         <Header></Header>
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/NotFound" element={<NotFoundPage />} />
         </Routes>
         <SwiperRes width={windowSize[0]}></SwiperRes>
         <Footer></Footer>
