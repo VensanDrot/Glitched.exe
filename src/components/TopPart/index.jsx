bb744fc792e8e018f8ff8a0c3672ed1222ac95fe
VensanDrot <mrvenchannel@gmail.com>
2023-02-11 11:54:14 -0500

    Video back

--- src/components/TopPart/index.jsx
+++ src/components/TopPart/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./index.css";
 import video from "../Assets/glihed_exe.webm";
+import video1 from "../Assets/glihed_exe1.mp4";
 
 const TopPart = () => {
   return (
     <div className="parent">
       <video className="b_v" autoPlay loop muted playsInline>
         <source src={video} type="video/webm" />
+        <source src={video1} type="video/mp4" />
+        Your browser does not support the video tag.
       </video>
       <div className="hold">
         <div className="h1">
